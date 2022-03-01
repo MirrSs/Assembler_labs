820b7dddd69f6f53781f0e18063436723be8cad0
MirrSs <75255156+MirrSs@users.noreply.github.com>
2022-03-01 18:22:47 +0300

    Update lab1_3_2.asm

--- lab1/lab1_3_2.asm
+++ lab1/lab1_3_2.asm
@@ -3,14 +3,14 @@
 .stack 100h
 .code
 start:
-  mov ax,DGROUP
+  mov ax,DGROUP    ;сегментный адрес строки message
   mov ds,ax
   mov dx,offset message
   mov ah,9
   int 21h
   mov dx,offset second_message
   int 21h
-  mov ax,4C00h
+  mov ax,4C00h   ;системный вызов завершения программы для .exe
   int 21h
 .data
 message db "Hello World!",0Dh,0Ah,'$'
